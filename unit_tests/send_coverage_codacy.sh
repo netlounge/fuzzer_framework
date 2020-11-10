3c9aca741106d5368b5b8f69ce3d91bdf2bb8e7a
Attila <Balazs>
2020-11-10 21:17:12 +0100

    configure sphinx

--- unit_tests/send_coverage_codacy.sh
+++ unit_tests/send_coverage_codacy.sh
@@ -1,5 +1,5 @@
-#!/bin/zsh
+#!/bin/bash
 export CODACY_PROJECT_TOKEN=da6e3d0e9ed646a1adf7615771f4e13e
-zsh <(curl -Ls https://coverage.codacy.com/get.sh) report
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report
 ls
-zsh <(curl -Ls https://coverage.codacy.com/get.sh) report -l Python -r coverage.xml
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report -l Python -r coverage.xml
