dde33959704a2d0b0d66999ee929474df442ca22
Attila <Balazs>
2021-05-05 17:16:00 +0200

    rm tk

--- framework/send_coverage_codacy.sh
+++ framework/send_coverage_codacy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CODACY_PROJECT_TOKEN=da6e3d0e9ed646a1adf7615771f4e13e
+
 bash <(curl -Ls https://coverage.codacy.com/get.sh) report
 ls
 bash <(curl -Ls https://coverage.codacy.com/get.sh) report -l Python -r coverage.xml
